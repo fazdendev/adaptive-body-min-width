10f797d284096c6ba74b1ff527edc2f9fa7a73d6
fazdendev <fazdendev@gmail.com>
2022-09-17 21:16:10 +0500

    Specific browser function was backuped
    As it turned out, the new version of the code does not work in mobile Firefox browser, and therefore a rollback was made to the previous one,
    
    Функция запуска в определенных браузерах была откачена на версию назад
    Как оказалось, новая версия кода не работает в мобильном браузере Firefox в связи с чем был сделан откат на предыдущую.

--- function.js
+++ function.js
@@ -34,28 +34,33 @@ function adaptiveSizePageScaleInit(definedStartWidth) {
 }
 function startOnSpecificBrowserInit() {
   let userAgent = window.navigator.userAgent.toLowerCase();
-  let browser = "other";
-  const browserNames = {
-    safari: "safari",
-    firefox: "firefox",
-    trident: "ie",
-    chrome: "chrome",
-    opr: "opera",
-    "edg/": "chrEdge",
-    edge: "msEdge"
-  };
-
-  for (const browserId in browserNames) {
-    if (userAgent.includes(browserId)) {
-      browser = browserNames[browserId];
-    }
-
-    if (browser == 'opera' && !window.opr || browser == 'chrome' && !window.chrome) {
-      browser = 'other'
-    }
+  let browser;
+  switch (true) {
+    case userAgent.indexOf("edge") > -1:
+      browser = "msEdge";
+      break;
+    case userAgent.indexOf("edg/") > -1:
+      browser = "chrEdge";
+      break;
+    case userAgent.indexOf("opr") > -1 && !!window.opr:
+      browser = "opera";
+      break;
+    case userAgent.indexOf("chrome") > -1 && !!window.chrome:
+      browser = "сhrome";
+      break;
+    case userAgent.indexOf("trident") > -1:
+      browser = "ie";
+      break;
+    case userAgent.indexOf("firefox") > -1:
+      browser = "firefox";
+      break;
+    case userAgent.indexOf("safari") > -1:
+      browser = "safari";
+      break;
+    default:
+      browser = "other";
   }
-
-  if (["safari", "firefox"].includes(browser)) {
+  if (browser == "safari" || browser == "firefox") {
     adaptiveSizePageScaleInit();
   }
 }
